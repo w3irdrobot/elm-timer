4c781439acd2125f440cb72c9c2c6392a55b7680
Alex Sears <me@alexsears.com>
2016-09-22 11:34:50 -0400

    Added nulls to be passed into Elm from JS side.

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,18 @@ require('../index.html');
 const Elm = require('../elm/Timer.elm');
 const main = document.getElementById('main');
 
-const timer = Elm.Timer.embed(main, {
-  seconds: 100000,
-  // hoursThreshold: 48,
-  formatUnderThreshold: '{H}::{M}::{S}',
+const nulls = {
+  hoursThreshold: null,
+  formatUnderThreshold: null,
+  formatOverThreshold: null
+};
+
+const flags = {
+  seconds: 500000,
   formatOverThreshold: 'Yo. It\'s gonna start pretty damn soon...{D} days!'
-});
+};
+
+const timer = Elm.Timer.embed(main, Object.assign({}, nulls, flags));
 
 timer.ports.timerExpired.subscribe((message) => {
   console.log(`Well, hello, sir. We shall ${message}!`);
